4bb57a1bef6f3dc95fc23bc741fe3889bdd91a53
Beathan Andersen <beathan.andersen@gmail.com>
2019-08-12 09:38:52 -0500

    Update with settings and dynamic sizing

--- PanelSelect.py
+++ PanelSelect.py
@@ -1,16 +1,24 @@
 import sublime
 import sublime_plugin
 
-
 class PanelSelectCommand(sublime_plugin.WindowCommand):
    """ This plugin allows easy changing of panel focus """
 
-   current_panel = 1
+   def __init__(self, window):
+      super().__init__(window)
 
-   layout3 = {'cells': [[0, 0, 1, 1], [1, 0, 2, 1], [2, 0, 3, 1]], 'rows': [0.0, 1.0], 'cols': [0.0, 0.15, 0.30, 1.0]}
-   layout2 = {'cells': [[0, 0, 1, 1], [1, 0, 2, 1], [2, 0, 3, 1]], 'rows': [0.0, 1.0], 'cols': [0.0, 0.15, 0.85, 1.0]}
-   layout1 = {'cells': [[0, 0, 1, 1], [1, 0, 2, 1], [2, 0, 3, 1]], 'rows': [0.0, 1.0], 'cols': [0.0, 0.7, 0.85, 1.0]}
    def run(self, panel=None):
+      """ Main run function """
+      size = get("panel_size")
+      self.layout3 = {'cells': [[0, 0, 1, 1], [1, 0, 2, 1], [2, 0, 3, 1]], 'rows': [0.0, 1.0], 'cols': [0.0, size, size*2, 1.0]}
+      self.layout2 = {'cells': [[0, 0, 1, 1], [1, 0, 2, 1], [2, 0, 3, 1]], 'rows': [0.0, 1.0], 'cols': [0.0, size, 1.0-size, 1.0]}
+      self.layout1 = {'cells': [[0, 0, 1, 1], [1, 0, 2, 1], [2, 0, 3, 1]], 'rows': [0.0, 1.0], 'cols': [0.0, 1.0-(size*2), 1.0-size, 1.0]}
+
+      debug ("Panel size is: " + str(size))
+      if size > 0.5:
+         debug ("Panel size is too large, must be less than 0.5")
+      
+      # select which panel layout to use
       if int(panel) is 3:
          self.window.set_layout(self.layout3)
          self.window.focus_group(2)
@@ -20,3 +28,32 @@ class PanelSelectCommand(sublime_plugin.WindowCommand):
       elif int(panel) is 1:
          self.window.set_layout(self.layout1)
          self.window.focus_group(0)
+
+
+SETTINGS_FILE = "PanelSelect.sublime-settings"
+GLOBAL_PREFERENCES = "Preferences.sublime-settings"
+class Settings:
+    """ Interface for communicating with settings """
+    plugin = None
+
+    def load():
+        """Loads the settings for the plugin"""
+        Settings.plugin = sublime.load_settings(SETTINGS_FILE)
+ 
+    def get(name):
+        """ Gets a value from the plugin settings """
+        if not Settings.plugin:
+            Settings.load()
+
+        plugin = Settings.plugin
+        return plugin.get(name)
+
+
+def get(name):
+    """ Gets a value from settings class """
+    return Settings.get(name)
+
+def debug(message):
+   """ Print a message to the console """
+   if get("debug") is True:
+      print (message)
